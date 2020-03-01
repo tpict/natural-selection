76c0126cd709787d7258b521610f41fc1698ee59
Tom Picton <tom@tompicton.com>
2020-02-29 17:15:49 -0800

    Add DatePicker test coverage

--- cypress/integration/natural-selection/date_picker.spec.tsx
+++ cypress/integration/natural-selection/date_picker.spec.tsx
@@ -1,10 +1,67 @@
+const mockdate = require("mockdate");
+
+before(() => {
+  mockdate.set("2020-02-26");
+});
+
+after(() => {
+  mockdate.reset();
+});
+
 beforeEach(() => {
   cy.visit("/date-picker");
 });
 
-it("opens the picker on click", () => {
+it("opens/closes the picker on click", () => {
   // TODO fix this force, Cypress thinks the placeholder is covering it
   cy.queryByLabelText("Date picker example").click({ force: true });
   cy.queryByText("February").should("exist");
   cy.queryByText("2020").should("exist");
+  cy.queryByLabelText("Date picker example").click({ force: true });
+  cy.queryByText("February").should("not.exist");
+  cy.queryByText("2020").should("not.exist");
+});
+
+it("picks a day and closes on click", () => {
+  // TODO fix this force, Cypress thinks the placeholder is covering it
+  cy.queryByLabelText("Date picker example").click({ force: true });
+  cy.queryByText("8").click();
+  cy.queryByText("02/08/2020").should("exist");
+  cy.queryByText("February").should("not.exist");
+});
+
+it("picks a day and closes on space", () => {
+  // TODO fix this force, Cypress thinks the placeholder is covering it
+  cy.queryByLabelText("Date picker example").click({ force: true });
+  cy.queryByText("8").trigger("mouseover");
+  cy.queryByLabelText("Date picker example").type(" ", { force: true });
+  cy.queryByText("02/08/2020").should("exist");
+  cy.queryByText("February").should("not.exist");
+});
+
+it("switches months on click", () => {
+  // TODO fix this force, Cypress thinks the placeholder is covering it
+  cy.queryByLabelText("Date picker example").click({ force: true });
+  cy.queryByText("February").click();
+  cy.queryByText("March").click();
+  cy.queryByText("February").should("not.exist");
+  cy.queryByText("March").should("exist");
+});
+
+it("switches years on click", () => {
+  // TODO fix this force, Cypress thinks the placeholder is covering it
+  cy.queryByLabelText("Date picker example").click({ force: true });
+  cy.queryByText("2020").click();
+  cy.queryByText("2021").click();
+  cy.queryByText("2020").should("not.exist");
+  cy.queryByText("2021").should("exist");
+});
+
+it("focuses options on text input", () => {
+  // TODO fix this force, Cypress thinks the placeholder is covering it
+  cy.queryByLabelText("Date picker example").click({ force: true });
+  cy.queryByLabelText("Date picker example").type("august 18th 1994{enter}", {
+    force: true,
+  });
+  cy.queryByText("08/18/1994").should("exist");
 });
