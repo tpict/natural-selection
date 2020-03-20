9d44122801e9006e8cc3196c42c828f79d4cbbcc
Tom Picton <tom@tompicton.com>
2020-03-19 19:14:27 -0700

    Set up coverage reporting

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,28 @@
 module.exports = function(api) {
   api.cache(true);
 
+  const { CODE_COVERAGE } = process.env;
+  const plugins = [
+["istanbul", {
+          "exclude": [
+            "**/*.spec.tsx"
+          ],
+}],
+    "@babel/proposal-object-rest-spread",
+    "@babel/proposal-optional-chaining",
+    "@babel/proposal-nullish-coalescing-operator",
+    "@emotion",
+  ];
+
+
   return {
     presets: [
       "@babel/preset-react",
+      "@babel/preset-env",
       "@babel/typescript",
-      [
-        "@babel/preset-env",
-        {
-          targets: {
-            browsers: ["last 2 versions"],
-          },
-        },
-      ],
       "@emotion/babel-preset-css-prop",
     ],
-    plugins: [
-      "@babel/proposal-object-rest-spread",
-      "@babel/proposal-optional-chaining",
-      "@babel/proposal-nullish-coalescing-operator",
-      "@emotion",
-    ],
+    plugins,
+    babelrc: false,
   };
 };
