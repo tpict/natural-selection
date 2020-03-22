698107b595247f5f99ea08ab7b0c014329e4d7b2
Tom Picton <tom@tompicton.com>
2020-03-21 22:38:25 -0700

    Fix CNAME file loss on publish

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path')
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -11,6 +12,9 @@ module.exports = merge(common, {
       staticDir: path.join(__dirname, 'dist'),
       // Required - Routes to render.
       routes: [ '/', '/test', '/examples', '/examples/single-select', '/examples/multi-select','/examples/date-picker'],
-    })
+    }),
+    new CopyPlugin([
+      { from: "CNAME", to: "." }
+    ])
   ]
 });
