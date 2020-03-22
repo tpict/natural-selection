aa101c56c7801f3cee61ee79037c691a43d59a9f
Tom Picton <tom@tompicton.com>
2020-03-21 22:32:05 -0700

    Set up site prerendering

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,16 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new PrerenderSPAPlugin({
+      // Required - The path to the webpack-outputted app to prerender.
+      staticDir: path.join(__dirname, 'dist'),
+      // Required - Routes to render.
+      routes: [ '/', '/test', '/examples', '/examples/single-select', '/examples/multi-select','/examples/date-picker'],
+    })
+  ]
 });
